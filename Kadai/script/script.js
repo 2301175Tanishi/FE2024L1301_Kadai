3835c870905f799a5ef50accb56b3db5a50cec00
2301175Tanishi <2301175@s.asojuku.ac.jp>
2024-10-28 12:32:36 +0900

    img

--- Kadai/script/script.js
+++ Kadai/script/script.js
@@ -15,36 +15,34 @@ new Vue({
         },
         hiku() {
             this.num -= this.count;
-            
         },
         kake() {
             this.num *= this.count;
-            
         },
         waru() {
             this.num /= this.count;
-            
         },
         reset() {
-            this.num,count = 0;
-            
+            this.num = 0;
+            this.count =0;
         },
         imageChange() {
             //HTMLからimage_areaの情報を取得する
-            
+            var mydiv = document.getElementById("image_area");
+            console.log(mydiv.innerHTML);
             //現在の数が0の場合「hime1.jfif」をimage_areaに表示する（<img src="./img/hime1.jfif" width="300">）
-            
-                
+            if(num == 0){
+                <img src="./img/hime1.jfif" width="300"></img>;
             //現在の数が3の倍数かつ5の倍数の場合「hime3.png」をimage_areaに表示する（<img src="./img/hime3.png" width="300">）
-            
-                
+            }else if(num / 3 == 0 && num / 5 ==0){
+                <img src="./img/hime3.png" width="300"></img>
             //現在の数が3の倍数または5の倍数の場合「hime2.jfif」をimage_areaに表示する（<img src="./img/hime2.jfif" width="300">）
-            
-                
+            }else if(num / 3 == 0 || num / 5 == 0){
+                <img src="./img/hime2.jfif" width="300"></img>
             //上記以外の場合「hime1.jfif」をimage_areaに表示する（<img src="./img/hime1.jfif" width="300">）
-            
-                
-            
+            }else{
+                <img src="./img/hime1.jfif" width="300"></img>
+            }
         }
     }
 });
