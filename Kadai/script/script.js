b29d394891062360e3413814f6ad67d4b72b6258
2301175Tanishi <2301175@s.asojuku.ac.jp>
2024-10-28 12:49:04 +0900

    いこーる

--- Kadai/script/script.js
+++ Kadai/script/script.js
@@ -12,37 +12,37 @@ new Vue({
            現在の数に応じて表示する画像を変更するメソッドを準備する */
         tasu() {
                 this.num += this.count;
-                imageChange() ;
+                imageChange();
         },
         hiku() {
             this.num -= this.count;
-            imageChange() ;
+            imageChange();
         },
         kake() {
             this.num *= this.count;
-            imageChange() ;
+            imageChange();
         },
         waru() {
             this.num /= this.count;
-            imageChange() ;
+            imageChange();
         },
         reset() {
             this.num = 0;
             this.count =0;
-            imageChange() ;
+            imageChange();
         },
         imageChange() {
             //HTMLからimage_areaの情報を取得する
-            var mydiv = document.getElementById("image_area");
+            const mydiv = document.getElementById("image_area");
             console.log(mydiv.innerHTML);
             //現在の数が0の場合「hime1.jfif」をimage_areaに表示する（<img src="./img/hime1.jfif" width="300">）
-            if(this.num == 0){
+            if(this.num === 0){
                 mydiv.innerHTML = '<img src="./img/hime1.jfif" width="300">';
             //現在の数が3の倍数かつ5の倍数の場合「hime3.png」をimage_areaに表示する（<img src="./img/hime3.png" width="300">）
-            }else if(this.num % 3 == 0 && num / 5 ==0){
+            }else if(this.num % 3 === 0 && num / 5 === 0){
                 mydiv.innerHTML = '<img src="./img/hime3.png" width="300">';
             //現在の数が3の倍数または5の倍数の場合「hime2.jfif」をimage_areaに表示する（<img src="./img/hime2.jfif" width="300">）
-            }else if(this.num % 3 == 0 || num / 5 == 0){
+            }else if(this.num % 3 === 0 || num / 5 === 0){
                 mydiv.innerHTML = '<img src="./img/hime2.jfif" width="300">';
             //上記以外の場合「hime1.jfif」をimage_areaに表示する（<img src="./img/hime1.jfif" width="300">）
             }else{
